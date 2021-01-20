06bdc1a6509970a535f5c6e8af15973fdab215ca
andrewbrodko <work.andrewbrodko@gmail.com>
2021-01-21 02:31:36 +0300

    Base page
    fetches data and redirects you to first available category page (assuming you have any categories)

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  async fetch({ store, redirect }) {
+    if (store.getters['items/categories'].length === 0) {
+      await store.dispatch('items/fetchCategories')
+    }
+    redirect(`/${store.getters['items/categories'][0].id}`)
+  },
+  computed: {
+    categories() {
+      return this.$store.getters['items/categories']
+    },
+  },
+  methods: {
+    openCategory(id) {
+      this.$router.push(`${id}`)
+    },
+  },
+}
+</script>
